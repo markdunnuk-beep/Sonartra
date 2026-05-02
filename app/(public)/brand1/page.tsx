e2bda4ebd11b4ee470c207eecf54eabd9b9ba06a
Mark Dunn <mark.dunn.uk@gmail.com>
2026-05-02 16:31:34 +0100

    Lock Sonartra brand accent and refine brand background

--- app/(public)/brand1/page.tsx
+++
@@ -1,5 +0,0 @@
-import { BRAND_ACCENTS, BrandPageContent } from '../brand/brand-page-content';
-
-export default function BrandTealDeepComparisonPage() {
-  return <BrandPageContent accent={BRAND_ACCENTS.tealDeep} />;
-}
--- app/(public)/brand2/page.tsx
+++
@@ -1,5 +0,0 @@
-import { BRAND_ACCENTS, BrandPageContent } from '../brand/brand-page-content';
-
-export default function BrandVioletComparisonPage() {
-  return <BrandPageContent accent={BRAND_ACCENTS.violet} />;
-}
